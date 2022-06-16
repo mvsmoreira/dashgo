5838b086f1902e518281abc614fc65ed77fc520b
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-16 17:41:53 -0300

    chore(env): configurations for chakra ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,22 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { theme } from "../styles/theme";
 
 export default class MyDocument extends Document {
-    render(): JSX.Element {
-        return (
-            <Html>
-                <Head>
-
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+              <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+              </Head>
+              <body>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <Main />
+                <NextScript />
+              </body>
             </Html>
-        )
-    }
+            )
+  }
 }
