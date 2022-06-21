819c6c1c37417d8e935fbac79e239fc65c169881
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-21 19:27:43 -0300

    feat(component): add UserList component

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,72 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react"
+import { RiAddLine, RiPencilLine } from "react-icons/ri"
+import Header from "../../components/Header"
+import Sidebar from "../../components/Sidebar"
+
+const UserList = () => {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8" />
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicius Moreira</Text>
+                    <Text fontSize="sm" color="gray.300">mvs.moreira93@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>21 de Junho de 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="yellow"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default UserList
