b89a264cc38ec12aa2bd4dc3e66311efb2889fc7
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-21 15:41:12 -0300

    feat(page): add SignIn page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,15 +8,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-              <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-              </Head>
-              <body>
-                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <Main />
-                <NextScript />
-              </body>
-            </Html>
-            )
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,43 @@
-const Home = () => {
+import { Button, Flex, Stack} from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+const SignIn = () => {
+
   return (
-    <h1>Hello</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+
+          <Input name='email' label='E-mail' type='email' />
+
+          <Input name='password' label='Senha' type='password' />
+
+        </Stack>
+
+        <Button
+          type='submit'
+          mt="6"
+          colorScheme="pink"
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
