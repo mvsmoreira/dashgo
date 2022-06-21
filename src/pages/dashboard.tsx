a08d1788ee4422042df42d0cffe0a4624781955b
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-21 18:41:51 -0300

    feat(component): add charts into Dashboard component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,64 @@
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react"
+import dynamic from "next/dynamic"
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
-import Chart from 'react-apexcharts'
+import { theme } from "../styles/theme"
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2022-06-18T00:00:00.000Z',
+      '2022-06-19T00:00:00.000Z',
+      '2022-06-20T00:00:00.000Z',
+      '2022-06-21T00:00:00.000Z',
+      '2022-06-22T00:00:00.000Z',
+      '2022-06-23T00:00:00.000Z',
+      '2022-06-24T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  }
+}
+
+const series = [{
+  name: 'series1',
+  data: [31, 120, 10, 25, 54, 108, 45]
+}]
 
 const Dashboard = () => {
   return (
@@ -19,14 +76,16 @@ const Dashboard = () => {
           // pb="4"
           >
             <Text fontSize="large" mb="4">Inscritos da semana</Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
           <Box
             p="8"
             bg="gray.800"
             borderRadius={8}
-          // pb="4"
+            pb="4"
           >
             <Text fontSize="large" mb="4">Taxa de abertura</Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
         </SimpleGrid>
