470181524c4e1c1589086b3cdcdd99e825f89ee3
Vinicius Moreira <mvs.moreira93@gmail.com>
2022-06-21 17:29:28 -0300

    chore(package): add apexcharts @3.35.3

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
-import { Flex } from "@chakra-ui/react"
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react"
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar"
+import Chart from 'react-apexcharts'
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,26 @@ const Dashboard = () => {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignContent="flex-start">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          // pb="4"
+          >
+            <Text fontSize="large" mb="4">Inscritos da semana</Text>
+          </Box>
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+          // pb="4"
+          >
+            <Text fontSize="large" mb="4">Taxa de abertura</Text>
+          </Box>
+
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
